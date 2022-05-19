26ebce4920c131b19f3801abcb51fa2223209764
AndresLubo <alubo_08@outlook.com>
2022-05-18 19:25:12 -0500

    Entrenamiento del modelo con validación cruzada, usando un unico modelo

--- main.py
+++ main.py
@@ -19,54 +19,44 @@ url = 'bank-full.csv'
 data = pd.read_csv(url)
 
 # Tratamiento de la data
+data.marital.replace(['married', 'divorced', 'single'], [0, 1, 2], inplace=True)
+data.education.replace(['primary', 'secondary', 'tertiary', 'unknown'], [0, 1, 2, 3], inplace=True)
+data.default.replace(['no', 'yes'], [0, 1], inplace=True)
+data.housing.replace(['no', 'yes'], [0, 1], inplace=True)
+data.loan.replace(['no', 'yes'], [0, 1], inplace=True)
+data.poutcome.replace(['failure', 'other', 'success', 'unknown'], [0, 1, 2, 3], inplace=True)
 
-data.Sex.replace(['female', 'male'], [0, 1], inplace=True)
-data['Embarked'].replace(['S', 'C', 'Q'], [0, 1, 2], inplace=True)
-data.drop(['Cabin', 'PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
-data.Age.replace(np.nan, 30, inplace=True)
+data.drop(['job', 'balance', 'contact', 'day', 'month', 'duration', 'pdays', 'previous', 'campaign'], axis= 1, inplace=True)
+
+data.age.replace(np.nan, 41, inplace=True)
 rangos = [0, 8, 15, 18, 25, 40, 60, 100]
 nombres = ['1', '2', '3', '4', '5', '6', '7']
-data.Age = pd.cut(data.Age, rangos, labels=nombres)
-data.dropna(axis=0, how='any', inplace=True)
+data.age = pd.cut(data.age, rangos, labels=nombres)
+
+data.dropna(axis=0,how='any', inplace=True)
+data.y.replace(['no', 'yes'], [0, 1], inplace=True)
 
 # partir la data en dos
 
-data_train = data[:850]
-data_test = data[850:]
+data_train = data[:728]
+data_test = data[728:]
 
-x = np.array(data_train.drop(['Survived'], 1))
-y = np.array(data_train.Survived)  # 0 Murió 1 vivió
+x = np.array(data_train.drop(['y'], 1))
+y = np.array(data_train.y) # 0 NO 1 Si
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+x_test_out = np.array(data_test.drop(['y'], 1))
+y_test_out = np.array(data_test.y) # 0 No 1 Si
 
-x_test_out = np.array(data_test.drop(['Survived'], 1))
-y_test_out = np.array(data_test.Survived)  # 0 Murió 1 vivió
 # Regresión Logística
 
 
 # Seleccionar un modelo
 logreg = LogisticRegression(solver='lbfgs', max_iter=7600)
 
-# Entreno el modelo
-logreg.fit(x_train, y_train)
-
-# MÉTRICAS
-
-print('*' * 50)
-print('Regresión Logística')
-
-# Accuracy de Entrenamiento de Entrenamiento
-print(f'accuracy de Entrenamiento de Entrenamiento: {logreg.score(x_train, y_train)}')
-
-# Accuracy de Test de Entrenamiento
-print(f'accuracy de Test de Entrenamiento: {logreg.score(x_test, y_test)}')
-
-# Accuracy de Validación
-print(f'accuracy de Validación: {logreg.score(x_test_out, y_test_out)}')
-
 # REGRESIÓN LOGÍSTICA CON VALIDACIÓN CRUZADA
 
-kfold = KFold(n_splits=5)
+kfold = KFold(n_splits=10)
 
 acc_scores_train_train = []
 acc_scores_test_train = []
@@ -110,6 +100,4 @@ print(f'Re-call: {recall}')
 f1_score = f1_score(y_test_out, y_pred, average=None).mean()
 
 print(f'f1: {f1_score}')
-
-
 
